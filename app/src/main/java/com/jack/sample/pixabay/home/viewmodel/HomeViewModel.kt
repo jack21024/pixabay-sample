382930795413771ac35bcc74bbfadef4949e7f02
toosakarin <jack21024@gmail.com>
2020-08-14 05:29:28 +0800

    [home] refactor: remove unused code.

--- app/src/main/java/com/jack/sample/pixabay/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/jack/sample/pixabay/home/viewmodel/HomeViewModel.kt
@@ -2,33 +2,23 @@ package com.jack.sample.pixabay.home.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import androidx.paging.PagedList
 import com.jack.baselibrary.viewmodel.BaseViewModel
 import com.jack.sample.pixabay.home.ui.recyclerview.item.MediumCardItem
 import com.jack.sample.pixabay.home.data.repository.MediumRepository
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class HomeViewModel(private val mediumRepository: MediumRepository) : BaseViewModel() {
 
-    private var _mediumLiveData: MutableLiveData<LiveData<PagedList<MediumCardItem>>> = MutableLiveData()
-    val mediumLiveData = Transformations.map(_mediumLiveData) {
-        it
-    }
-
+    val mediumLiveData: MutableLiveData<LiveData<PagedList<MediumCardItem>>> = MutableLiveData()
 
     override suspend fun loadData() {
-        // get layout style first
-        delay(1000)
-
-        // searching
         searchMedium()
     }
 
     fun searchMedium(keyword: String? = null) {
         viewModelScope.launch {
-            _mediumLiveData.value = mediumRepository.getImageList(keyword)
+            mediumLiveData.value = mediumRepository.getImageList(keyword)
         }
     }
 }
