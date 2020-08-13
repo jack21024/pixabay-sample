450eb3130a30661bc66f9808019004c4c9e9fc5f
toosakarin <jack21024@gmail.com>
2020-08-13 21:53:35 +0800

    [home] fix placeholder not fitting medium image size.

--- app/src/main/java/com/jack/sample/pixabay/home/ui/recyclerview/viewerholder/MediumViewHolder.kt
+++ app/src/main/java/com/jack/sample/pixabay/home/ui/recyclerview/viewerholder/MediumViewHolder.kt
@@ -1,24 +1,36 @@
 package com.jack.sample.pixabay.home.ui.recyclerview.viewerholder
 
+import android.graphics.drawable.GradientDrawable
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.jack.baselibrary.recyclerview.viewholder.BaseViewHolder
+import com.jack.baselibrary.utils.DisplayUtils
 import com.jack.sample.pixabay.R
 import com.jack.sample.pixabay.home.ui.recyclerview.item.MediumCardItem
 import kotlinx.android.synthetic.main.item_medium.view.*
 
+
 class MediumViewHolder(parent: ViewGroup ) : BaseViewHolder<MediumCardItem>(R.layout.item_medium, parent) {
 
     override fun bind(data: MediumCardItem) {
+        val ratio = data.imageHeigh.toFloat() / data.imageWidth.toFloat()
+        val w = DisplayUtils.screenWidth
+        val h = Math.round(w.toFloat() * ratio)
+        val placeholder = itemView.context.getDrawable(R.drawable.draw_medium_placholder)?.apply {
+            (this as GradientDrawable).setSize(w, h)
+        }
         itemView.apply {
+            image_medium.setImageResource(0)
             text_user_name.text = data.userName
             text_likes.text = data.likes.toString()
             text_stars.text = data.stars.toString()
             text_comments.text = data.stars.toString()
             Glide.with(context)
                 .load(data.imageUrl)
-                .placeholder(R.drawable.draw_medium_placholder)
+                .placeholder(placeholder)
+                .override(w, h)
                 .into(this.image_medium)
         }
     }
